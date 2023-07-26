9c0792caa17f6c1d7696573e3542021e7173143b
Sidra Azam <sidraazam2016@gmail.com>
2023-07-26 15:47:36 +0500

    commit

--- cypress/e2e/Ryobi/Canopy UI.cy.js
+++ cypress/e2e/Ryobi/Canopy UI.cy.js
@@ -3,18 +3,18 @@
 it('CANOPY',() => {
     cy.visit('https://canopy.technyxconsulting.com/en/summer-big-win')
 
-    // //Next Button 1
-    // cy.get('.js-pong-start-btn-mbl').click()
+    //Next Button 1
+    cy.get('.js-pong-start-btn-mbl').click()
     
 
-    // //Next Button 2
-    // cy.get('.js-next').click()
+    //Next Button 2
+    cy.get('.js-next').click()
 
-    // //Next Button 3
-    // cy.get('.js-launch-game').click()
+    //Next Button 3
+    cy.get('.js-launch-game').click()
 
-    // //Game Controller
-    // cy.get('canvas')
+    //Game Controller
+    cy.get('canvas')
 
 
     
